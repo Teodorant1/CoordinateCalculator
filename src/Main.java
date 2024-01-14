750cc5ffa9f9b92eea51b482f631975a512405e1
Dusan <dusanbojanic1@gmail.com>
2024-01-15 00:05:56 +0100

    STUFF

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 
 import java.io.IOException;
 
-class Main {
+
+ class Main {
     public static void main(String[]args) throws IOException {
         DistanceCalculator distanceCalculator = new DistanceCalculator();
         distanceCalculator.calculate_distance( 10 , 20, 30, 40 );
